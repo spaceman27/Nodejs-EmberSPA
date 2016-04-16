a611cfc3836e9e52aba32b9b411220733b834ca8
Dao Nguyen <spaceman27@gmail.com>
2016-04-15 17:35:38 -0700

    update router and add index template

--- public/scripts/app/router.js
+++ public/scripts/app/router.js
@@ -1,6 +1,9 @@
 Router.map(function(){
-  this.route('index', { path: '/' });
-   this.route('candidateSearching', { path: '/candidateSearching' });
-
-   this.route('page-not-found', { path: '/*wildcard' });
+  this.route('index', { path: '/candidate' });
+   this.route('candidate', { path: '/candidate' });
+   this.route('employer', { path: '/employer' });
+   this.route('page-not-found', { path: '/*wildcard' });   
+});
+App.Router.reopen({
+  location: 'hash'
 });
