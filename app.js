c5c676cb8400ca44cc1a36796c60c5b771335c82
Dao Nguyen <spaceman27@gmail.com>
2016-04-17 18:57:11 -0700

    add service, view

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ var express = require("express");
 var app = express();
 
 var port = 3000;
+
 app.use(express.static('public'));
+app.set('view', './src/views');
+var handlebars = require('express-handlebars');
+app.engine('.hbs', handlebars({extname: '.hbs'}));
+
+
 app.get('/', function(req, res){
 	res.send('Hello world');
 });
---
+++ src/services/resumeService.js
@@ -0,0 +1,23 @@
+var resumeService = function(){
+	// read json here
+	var getJson = function(callback){
+		// call mongodb get result
+		var result = null;
+		var err = null;
+		
+		var options = {
+			host: '/',
+			path: 'resume.json'
+		};
+		http.request(options, function(response){
+			var jsonStr='';
+			response.on('data', function(chunk){
+				jsonStr += chunk;
+			}).on(end, function(){
+				console.log(str);
+				callback(null, jsonStr);
+			})
+		}).end();
+	}
+}
+module.exports = resumeService;
