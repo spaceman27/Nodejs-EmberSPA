e665ad5e0bc5eb2216a59c03c90887d17605de3b
Dao Nguyen <spaceman27@gmail.com>
2016-04-20 20:52:28 -0700

    add app.js for routing tie

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ var app = express();
 var port = 3000;
 
 app.use(express.static('public'));
-app.set('view', './src/views');
+
+app.set('views', './src/views');
 var handlebars = require('express-handlebars');
 app.engine('.hbs', handlebars({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
-
-app.get('/', function(req, res){
-	res.send('Hello world');
-});
-
+var indexRouter = require('./src/routes/indexRoutes');
+app.use('/', indexRouter);
 app.listen(port, function(err){
 	console.log("Welcome to NodeCrash Project on port: " + port );
 });
